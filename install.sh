9effa2a36794c141554c5ba0a4fafa569540b598
MohithReddy <m.mohithreddy99@gmail.com>
2018-03-24 23:03:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ chmod 755 /usr/local/bin/proxifier
 mkdir -p /opt/proxifier/bin /opt/proxifier/bak /opt/proxifier/conf /opt/proxifier/buffer
 
 cp executables/* /opt/proxifier/bin/
+chmod 755 /opt/proxifier/bin/*
 
 
 echo "\nEnter the proxydetails , It creates proxyserver.conf file in /opt/proxifier/conf directory you can created mutiple files as proxifier.conf.<profilename> and run that profile as proxifier.conf.<profilename>\n"
