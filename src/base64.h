8bd95efd2254f2742ef41797701aa191a3762dd1
Mohith Reddy <m.mohithreddy99@gmail.com>
2020-05-19 02:02:01 +0530

    Deleted files for further modifications

--- src/base64.h
+++
@@ -1,4 +0,0 @@
-void decodeblock(unsigned char in[], char *clrstr);
-void decodepass(char *b64src, char *clrdst);
-void encodeblock( unsigned char in[], char b64str[], int len );
-void encodepass(char *clrstr, char *b64dst);
--- src/socket.h
+++
@@ -1,5 +0,0 @@
-#include<netinet/in.h>
-
-struct sockaddr_in createstructsockaddr_in(int family,char* ipv4,int port);
-int createsocket(int family,int type,char *ipv4,int port);
-int max(int a,int b);
