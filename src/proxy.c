5af856167aec9b897ff541c943c1860c7a4ccaaf
Mohith Reddy <m.mohithreddy99@gmail.com>
2020-05-23 15:17:27 +0530

    Structures and functions for proxy handlers and proxy clients

--- src/proxy.c
+++ src/proxy.c
@@ -1,78 +1,205 @@
-#include"proxy.h"
-#include<string.h>
-#include<stdlib.h>
-#include<stdio.h>
+/*
+ * proxy.c
+ *
+ *  Created on: 16-May-2020
+ *      Author: Mohith Reddy
+ */
 
-int getproxyserverip(char *fileinstring,char *proxyip)
+#include "proxy.h"
+#include "proxy_functions.h"
+#include "proxy_socket.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <pthread.h>
+
+struct in_addr* get_lo_interface_in_addr()
 {
-	FILE *fp=fopen(fileinstring,"r");
-	char buffer[100],*counterpointer;
-	counterpointer=fgets(buffer,99,fp);
-	while(counterpointer!=NULL)
-	{
-		if(strstr(buffer,"ProxyServerIP")!=NULL)
-		{
-			char *temp=strstr(buffer,"=");
-			temp++;
-			temp[strlen(temp)-1]='\0';
-			strcpy(proxyip,temp);
-			break;
-		}
-		counterpointer=fgets(buffer,99,fp);
-	}
-	if(strlen(proxyip)==0)
-	{
-		printf("ProxyServerIP is wrongly configured in %s\n",fileinstring);
-		exit(-1);
-	}
-	return 0;
+	struct in_addr* lo_in_addr = (struct in_addr*) calloc(1, sizeof(struct in_addr));
+
+	lo_in_addr->s_addr = htonl(INADDR_LOOPBACK);
+
+	return lo_in_addr;
 }
 
-int getproxyserverport(char *fileinstring)
+struct proxy_options* create_proxy_options(struct proxy_options* px_opt)
 {
-	int port;
-	FILE *fp=fopen(fileinstring,"r");
-	char buffer[100],portinstring[10],*counterpointer;
-	bzero(&portinstring,10);
-	counterpointer=fgets(buffer,99,fp);
-	while(counterpointer!=NULL)
-	{
-		if(strstr(buffer,"ProxyServerPort")!=NULL)
-		{
-			char *temp=strstr(buffer,"=");
-			temp++;
-			temp[strlen(temp)-1]='\0';
-			strcpy(portinstring,temp);
-			break;
-		}
-		counterpointer=fgets(buffer,99,fp);
+	if (px_opt == NULL)
+		return NULL;
+
+	struct proxy_options* dup_opt = (struct proxy_options*) calloc(1, sizeof(struct proxy_options));
+
+	if (dup_opt == NULL)
+		return NULL;
+
+	/* Proxy Server information */
+
+	if (px_opt->px_server != NULL) {
+		dup_opt->px_server = strdup(px_opt->px_server);
+
+		if (dup_opt->px_server == NULL)
+			return NULL;
 	}
-	if(strlen(portinstring)==0)
-	{
-		printf("ProxyServerPort is wrongly configured in %s\n",fileinstring);
-		exit(-1);
+
+	if (px_opt->px_port != NULL) {
+		dup_opt->px_port = strdup(px_opt->px_port);
+
+		if (dup_opt->px_port == NULL)
+			return NULL;
 	}
-	else
-		port=atoi(portinstring);
-	return port;
+
+	if (px_opt->px_username != NULL) {
+		dup_opt->px_username = strdup(px_opt->px_username);
+
+		if (dup_opt->px_username == NULL)
+			return NULL;
+	}
+
+	if (px_opt->px_password != NULL) {
+		dup_opt->px_password = strdup(px_opt->px_password);
+
+		if (dup_opt->px_password == NULL)
+			return NULL;
+	}
+
+	/* Network Variables */
+
+	dup_opt->io_timeout = px_opt->io_timeout;
+
+	/* Signal variables */
+
+	if (px_opt->sigmask != NULL) {
+		dup_opt->sigmask = memndup(px_opt->sigmask, sizeof(sigset_t));
+
+		if (dup_opt->sigmask == NULL)
+			return NULL;
+	}
+
+	dup_opt->signo = px_opt->signo;
+
+	return dup_opt;
 }
 
-int getproxyserverauthorization(char *fileinstring,char *proxypass)
+int free_proxy_options(struct proxy_options** px_opt)
 {
-	FILE *fp=fopen(fileinstring,"r");
-	char buffer[100],*counterpointer;
-	counterpointer=fgets(buffer,99,fp);
-	while(counterpointer!=NULL)
-	{
-		if(strstr(buffer,"ProxyServerAuthorization")!=NULL)
-		{
-			char *temp=strstr(buffer,"=");
-			temp++;
-			temp[strlen(temp)-1]='\0';
-			strcpy(proxypass,temp);
-			break;
-		}
-		counterpointer=fgets(buffer,99,fp);
+	if (px_opt == NULL || *px_opt == NULL)
+		return PROXY_ERROR_INVAL;
+
+	/* Free pointers */
+
+	free((*px_opt)->px_server);
+	free((*px_opt)->px_port);
+	free((*px_opt)->px_username);
+	free((*px_opt)->px_password);
+	free((*px_opt)->sigmask);
+
+	/* Free px_opt{} */
+
+	free(*px_opt);
+	*px_opt = NULL;
+
+	return PROXY_ERROR_NONE;
+}
+
+struct proxy_handler* create_proxy_handler()
+{
+	struct proxy_handler* px_handler = (struct proxy_handler*) calloc(1, sizeof(struct proxy_handler));
+
+	px_handler->px_opt = (struct proxy_options*) calloc(1, sizeof(struct proxy_options));
+
+	px_handler->px_opt->px_server = "192.168.43.231";
+	px_handler->px_opt->px_port = "3128";
+	px_handler->px_opt->px_username = NULL;
+	px_handler->px_opt->px_password = NULL;
+	px_handler->px_opt->nrd_ports = 2;
+	px_handler->px_opt->rd_ports = malloc(sizeof(char*) * px_handler->px_opt->nrd_ports);
+	px_handler->px_opt->rd_ports[0] = "80";
+	px_handler->px_opt->rd_ports[1] = "443";
+	/* Network-Variables */
+	px_handler->px_opt->io_timeout = 60;
+	/* Signal Variables */
+	px_handler->px_opt->signo = SIGRTMIN;
+	px_handler->px_opt->sigmask = malloc(sizeof(sigset_t));
+	sigemptyset(px_handler->px_opt->sigmask);
+	sigaddset(px_handler->px_opt->sigmask, px_handler->px_opt->signo);
+
+	px_handler->quit = 0;
+	px_handler->proto_data = NULL;
+
+	return px_handler;
+}
+
+struct proxy_request* create_proxy_request(struct proxy_handler* px_handler, protocol_data_setup proto_data_setup)
+{
+	if (px_handler == NULL)
+		return NULL;
+
+	struct proxy_request* px_request = (struct proxy_request*) calloc(1, sizeof(struct proxy_request));
+
+	/* Duplicate proxy_options{} */
+
+	if (px_handler->px_opt != NULL) {
+		px_request->px_opt = create_proxy_options(px_handler->px_opt);
+
+		if (px_request->px_opt == NULL)
+			return NULL;
 	}
-	return 0; 
+
+	/* Thread Variables */
+
+	px_request->ptid = pthread_self();
+	px_request->px_client = NULL;
+
+	/* Variables for Synchronization */
+
+	px_request->quit = 0;
+
+	/* Variables for socket operations */
+
+	px_request->sockfd = -1;
+	memset(&px_request->addr, 0, sizeof(struct sockaddr_storage));
+	px_request->addr_len = sizeof(struct sockaddr_storage);
+
+	/* Protocol specific data */
+
+	if (proto_data_setup != NULL) {
+		if ((*proto_data_setup)(px_handler, &(px_request->proto_data)) != PROXY_ERROR_NONE)
+			return NULL;
+	}
+
+	return px_request;
+}
+
+int free_proxy_request(struct proxy_request** px_request, protocol_data_free proto_data_free)
+{
+	if (px_request == NULL || *px_request == NULL)
+		return PROXY_ERROR_INVAL;
+
+	int free_status = PROXY_ERROR_NONE;
+
+	/* Free pointers */
+
+	if ((*px_request)->px_opt != NULL) {
+		if (free_proxy_options(&((*px_request)->px_opt)) != PROXY_ERROR_NONE)
+			free_status = PROXY_ERROR_INVAL;
+	}
+
+	if ((*px_request)->px_client != NULL) {
+		if (free_proxy_client(&((*px_request)->px_client)) != PROXY_ERROR_NONE)
+			free_status = PROXY_ERROR_INVAL;
+	}
+
+	if (proto_data_free != NULL && (*px_request)->proto_data != NULL) {
+		if ((*proto_data_free)(&((*px_request)->proto_data)) != PROXY_ERROR_NONE)
+			free_status = PROXY_ERROR_INVAL;
+	}
+
+	/* Free px_request{} */
+
+	free(*px_request);
+	*px_request = NULL;
+
+	return free_status;
 }
--- src/proxy.h
+++ src/proxy.h
@@ -1,3 +1,108 @@
-int getproxyserverip(char *fileinstring,char *proxyip);
-int getproxyserverport(char *fileinstring);
-int getproxyserverauthorization(char *fileinstring,char *proxypass);
+/*
+ * proxy.h
+ *
+ *  Created on: 18-May-2020
+ *      Author: Mohith Reddy
+ */
+
+#ifndef SRC_PROXY_H_
+#define SRC_PROXY_H_
+
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <signal.h>
+
+/* Proxy Constants */
+
+
+/* Proxy Errors */
+
+#define PROXY_ERROR_NONE 0
+#define PROXY_ERROR_RETRY 1
+#define PROXY_ERROR_FATAL 2
+#define PROXY_ERROR_INVAL 3
+#define PROXY_ERROR_TIMEOUT 4
+#define PROXY_ERROR_SIGRCVD 5
+#define PROXY_ERROR_BUFFER_FULL 6
+#define PROXY_ERROR_DNS 7
+
+/* Proxy Defaults */
+
+#define PROXY_DEFAULT_ACCEPT_BACKLOG 25
+
+struct in_addr* get_lo_interface_in_addr();
+
+#define PROXY_DEFAULT_SERVER_LISTENER inet_ntop(AF_INET, (void*) get_lo_interface_in_addr(), \
+		(char*) malloc(INET_ADDRSTRLEN), INET_ADDRSTRLEN)
+
+#define PROXY_DEFAULT_HTTP_METHOD "CONNECT"
+#define PROXY_DEFAULT_CONNECT_ADDRESS "127.0.0.1:80"
+#define PROXY_DEFAULT_HTTP_VERSION "1.1"
+
+/* Proxy Maxs */
+
+#define PROXY_MAX_TRANSACTION_SIZE 65536
+
+struct proxy_options {
+	/* Proxy-Server information */
+	char* px_server;
+	char* px_port;
+	char* px_username;
+	char* px_password;
+	/* Redirection variables */
+	char** rd_ports;
+	long nrd_ports;
+	/* Network-Variables */
+	long io_timeout;
+	/* Signal Variables */
+	sigset_t* sigmask;
+	int signo;
+};
+
+struct proxy_handler {
+	/* Proxy Settings */
+	struct proxy_options* px_opt;
+	/* Thread variables */
+	pthread_t ptid;
+	pthread_t tid;
+	struct proxy_client* pxl_server;
+	/* Variables for Synchronization */
+	int quit;
+	/* Protocol specific data */
+	void* proto_data;
+};
+
+struct proxy_request {
+	/* Proxy_Settings */
+	struct proxy_options* px_opt;
+	/* Thread variables */
+	pthread_t ptid;
+	pthread_t tid;
+	struct proxy_client* px_client;
+	/* Variables for Synchronization */
+	int quit;
+	/* Variables for socket operations */
+	int sockfd;
+	struct sockaddr_storage addr;
+	int addr_len;
+	/* Protocol specific data */
+	void* proto_data;
+};
+
+typedef int (*protocol_data_setup) (struct proxy_handler*, void**);
+
+typedef int (*protocol_data_free) (void**);
+
+struct proxy_options* create_proxy_options(struct proxy_options* px_opt);
+
+int free_proxy_options(struct proxy_options** px_opt);
+
+struct proxy_handler* create_proxy_handler();
+
+struct proxy_request* create_proxy_request(struct proxy_handler* px_handler, protocol_data_setup proto_data_setup);
+
+int free_proxy_request(struct proxy_request** px_request, protocol_data_free proto_data_free);
+
+#endif /* SRC_PROXY_H_ */
