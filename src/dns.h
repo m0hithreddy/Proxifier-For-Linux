62b6b60e1e29d169deb56b1ae03e15fa3080d6f2
Mohith Reddy <m.mohithreddy99@gmail.com>
2020-06-06 23:44:11 +0530

    Update DNS routines for general message parsing and making

--- src/dns.h
+++ src/dns.h
@@ -15,6 +15,7 @@
 #define DNS_MODE_COMPLETE 0b0
 
 #include "bit.h"
+#include "proxy_structures.h"
 #include <stdint.h>
 
 struct dns_header {
@@ -36,7 +37,7 @@ struct dns_header {
 struct dns_question {
 	void* qname;
 	unsigned int qname_len;
-	char* dname;
+	char* hname;
 	uint16_t qtype;
 	uint32_t qclass;
 };
@@ -44,6 +45,7 @@ struct dns_question {
 struct dns_rrecord {
 	void* name;
 	unsigned int name_len;
+	char* hname;
 	uint16_t type;
 	uint16_t class;
 	uint32_t ttl;
@@ -63,9 +65,23 @@ struct dns_msg {
 	uint16_t arcount;
 };
 
-struct proxy_data* create_dns_response(struct dns_msg* dns_response, int cd_flags);
+int dump_dns_header(struct dns_header* dns_hdr, struct proxy_data* dns_data, unsigned long* bit_start);
 
-struct dns_msg* parse_dns_query(struct proxy_data* dns_data, int pd_flags);
+int dump_dns_question(struct dns_question* dns_ques, struct proxy_data* dns_data, unsigned long* bit_start);
+
+int dump_dns_rrecord(struct dns_rrecord* dns_record, struct proxy_data* dns_data, unsigned long* bit_start);
+
+struct proxy_data* create_dns_msg(struct dns_msg* dns_msg, int cd_flags);
+
+int parse_dns_header(struct proxy_data* dns_data, struct dns_header* dns_hdr, unsigned long *bit_start);
+
+int parse_dns_question(struct proxy_data* dns_data, struct dns_question* dns_ques, unsigned long *bit_start);
+
+int parse_dns_rrecord(struct proxy_data* dns_data, struct dns_rrecord* dns_record, unsigned long* bit_start);
+
+struct dns_msg* parse_dns_msg(struct proxy_data* dns_data, int pd_flags);
+
+int domain_to_host(struct proxy_data* dns_data, unsigned long *bit_start, char** _hname);
 
 struct proxy_data* host_to_domain(char* hostname);
 
