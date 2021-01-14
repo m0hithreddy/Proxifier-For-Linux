e81f0152b732b50d02ba29f2f0f9036e1f149d83
Mohith Reddy <dev.m0hithreddy@gmail.com>
2021-01-14 15:52:59 +0530

    Update License

--- src/proxy_structures.h
+++ src/proxy_structures.h
@@ -1,19 +1,21 @@
-/* proxy_structures.c declarations. 
-
-Copyright (C) 2020  Mohith Reddy
-
-Proxifier is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Proxifier is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+/*******************************************************************************
+ * Copyright (C) 2020 - 2021, Mohith Reddy <dev.m0hithreddy@gmail.com>
+ *
+ * This file is part of Proxifier-For-Linux <https://github.com/m0hithreddy/Proxifier-For-Linux>
+ *
+ * Proxifier is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proxifier is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 
 #ifndef SRC_PROXY_STRUCTURES_H_
 #define SRC_PROXY_STRUCTURES_H_
